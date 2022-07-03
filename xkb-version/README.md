7eda2bfbb983fc31c9ce39a04e8f737f6cebe059
Iso Lee <coughingmouse@gmail.com>
2022-07-03 19:06:16 +0900

    ask

--- xkb-version/README.md
+++ xkb-version/README.md
@@ -1,6 +1,9 @@
 # p layout (xkb version)
-##### Implementation of the [P layouts](https://github.com/coughingmouse/p-layout)
-## This code replaces the us layout (and assumes it is being used) without backing up. You need internet connection to change it back. 
+
+An implementation of the [P layouts](https://github.com/coughingmouse/p-layout).
+
+***This code replaces the us layout (and assumes it is being used) without backing up. You need internet connection to change it back.***
+
 Note: This does not change your vt keyboard. (Usually...? But on Pop OS 21.10, it seems this also works on vt.) Especially a bit whoopsy on BSDs. Lastly and least importantly, layouts of level 3+ are not changed.
 
 * Requires a distro with X11 (or Wayland?)
@@ -9,10 +12,13 @@ Note: This does not change your vt keyboard. (Usually...? But on Pop OS 21.10, i
 * You are using us or us altgr-intl (and not us-intl or anything like that) layout
 
 The following "replaces" current keyboard with Qwerpy or the default us layout on X11 or Wayland.
+
 ```sh
 sh -c "$(curl -fsSL https://github.com/coughingmouse/p-layout/raw/main/xkb-version/install.sh)"
 ```
+
 In case the above does not run (i.e. if curl is not installed), try this instead:
+
 ```sh
 sh -c "$(wget -q https://github.com/coughingmouse/p-layout/raw/main/xkb-version/install.sh -O -)"
 ```
@@ -23,7 +29,6 @@ sh -c "$(wget -q https://github.com/coughingmouse/p-layout/raw/main/xkb-version/
 * 2021-11-16 Made it recover default us layout, install dependencies, kinda remove bash dependency, and set some xkb-options.
 * 2021-11-27 wget support added
 * 2021-11-28 applied better layout
-
 
 ## To do (The ones left now are likely not going to be done by the original author)
 
--- xkb-version/useful_settings.sh
+++ xkb-version/useful_settings.sh
@@ -4,26 +4,31 @@
 if [ "$(gsettings get org.gnome.desktop.input-sources xkb-options)" ]; then
 
   # Enable option to toggle language with caps toggle
-  xkb_options=$(gsettings get org.gnome.desktop.input-sources xkb-options)
-  if [ ! "$(echo "$xkb_options" | grep 'shift:breaks_caps')" ]; then
-    if [ "$xkb_options" = "@as []" ]; then
-      xkb_options="['shift:breaks_caps']"
-      gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
-    else
-      xkb_options="$(echo "$xkb_options" | sed 's/.$//'), 'shift:breaks_caps']"
-      gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
-    fi
-  fi
-  if [ ! "$(echo "$xkb_options" | grep 'grp:caps_toggle')" ]; then
-    if [ "$xkb_options" = "@as []" ]; then
-      xkb_options="['grp:caps_toggle']"
-      gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
-    else
-      xkb_options="$(echo "$xkb_options" | sed 's/.$//'), 'grp:caps_toggle']"
-      gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
-    fi
-    echo "From now, lock into another layout with the Generalized lock key, also known as CapsLock. Lock Caps with Shift + Generalized_lock instead. If you customized your lock key, this may not function as intended."
-  fi
+  echo "Do you want to toggle layout with caps toggle key? This works only when xkb does. (y/N)"
+  read -r REPLY
+  case $REPLY in
+    [Yy]* ) xkb_options=$(gsettings get org.gnome.desktop.input-sources xkb-options)
+            if [ ! "$(echo "$xkb_options" | grep 'shift:breaks_caps')" ]; then
+              if [ "$xkb_options" = "@as []" ]; then
+                xkb_options="['shift:breaks_caps']"
+                gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
+              else
+                xkb_options="$(echo "$xkb_options" | sed 's/.$//'), 'shift:breaks_caps']"
+                gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
+              fi
+            fi
+            if [ ! "$(echo "$xkb_options" | grep 'grp:caps_toggle')" ]; then
+              if [ "$xkb_options" = "@as []" ]; then
+                xkb_options="['grp:caps_toggle']"
+                gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
+              else
+                xkb_options="$(echo "$xkb_options" | sed 's/.$//'), 'grp:caps_toggle']"
+                gsettings set org.gnome.desktop.input-sources xkb-options "$xkb_options"
+              fi
+              echo "From now, lock into another layout with the Generalized lock key, also known as CapsLock. Lock Caps with Shift + Generalized_lock instead. If you customized your lock key, this may not function as intended."
+            fi;;
+    * ) ;;
+    esac
 
   ## to add Thinkpad X230-specific patch
   ## gsettings set org.gnome.desktop.input-sources xkb-options "['compose:prsc']" 
@@ -43,16 +48,21 @@ fi
 # Use fn key to access ibus emoji
 # Note that the value changed is actually at desktop.ibus.panel.emoji
 if [ "$(gsettings get org.freedesktop.ibus.panel.emoji hotkey)" ]; then
-  hotkey=$(gsettings get org.freedesktop.ibus.panel.emoji hotkey)
-  if [ ! "$(echo "$hotkey" | grep 'WakeUp')" ]; then
-    if [ "$hotkey" = "@as []" ]; then
-      hotkey="['WakeUp']"
-      gsettings set org.freedesktop.ibus.panel.emoji hotkey "$hotkey"
-    else
-      hotkey="$(echo "$hotkey" | sed 's/.$//'), 'WakeUp']"
-      gsettings set org.freedesktop.ibus.panel.emoji hotkey "$hotkey"
-    fi
-    echo "Now fn key acts as a emoji picker, too."
-  fi
+  echo "Do you want to Fn key for emoji? This works only when ibus does. (y/N)"
+  read -r REPLY
+  case $REPLY in
+    [Yy]* ) hotkey=$(gsettings get org.freedesktop.ibus.panel.emoji hotkey)
+            if [ ! "$(echo "$hotkey" | grep 'WakeUp')" ]; then
+              if [ "$hotkey" = "@as []" ]; then
+                hotkey="['WakeUp']"
+                gsettings set org.freedesktop.ibus.panel.emoji hotkey "$hotkey"
+              else
+                hotkey="$(echo "$hotkey" | sed 's/.$//'), 'WakeUp']"
+                gsettings set org.freedesktop.ibus.panel.emoji hotkey "$hotkey"
+              fi
+              echo "Now fn key acts as a emoji picker, too."
+            fi;;
+    * ) ;;
+  esac
 fi
 
